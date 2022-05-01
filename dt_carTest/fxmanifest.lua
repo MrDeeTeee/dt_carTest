6f52a6f266a582ded4d58cfe7274b984c2d7faa2
MrDeeTee <87229607+MrDeeTeee@users.noreply.github.com>
2022-05-01 17:09:40 +1000

    Add files via upload
    merged carTest and liveHandling

--- dt_carTest/fxmanifest.lua
+++ dt_carTest/fxmanifest.lua
@@ -5,7 +5,17 @@ author 'Codesign | MrDeeTee'
 description 'dt_carTest'
 version '1.0'
 
+ui_page 'html/index.html'
+
+files {
+	'html/index.css',
+	'html/index.html',
+	'html/index.js',
+}
+
 client_scripts {
+	'config.lua',
+	'debugger.lua',
 	'client.lua',
 	'functions.lua'
 }
